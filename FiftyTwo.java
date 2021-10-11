21494b7dbd49763fcebc07f61562c3e526800179
Eganathan R <ammasamrith@gmail.com>
2021-10-11 11:37:28 +0530

    Pattern 52

---
+++ FiftyTwo.java
@@ -0,0 +1,38 @@
+public class FiftyTwo extends FiftyEightPatterns{
+    //***** START OF PATTERN 50*****//
+    public static void Pattern(int input){
+    /*      1
+           2 3 2
+          4 5 6 5 4
+         7 8 9 10 9 8 7
+        11 12 13 14 15 14 13 12 11*/
+
+        System.out.println("***Printing the Pattern***");
+
+        int temp =1;
+        for(int p = 1; p <= input; p++){
+
+            for(int s=input-p; s >=1; s--){
+                System.out.print("  ");
+            }
+
+
+            for(int q=1; q <= p; q++){
+                System.out.print(temp +" ");
+                temp++;
+            }
+
+            int temp2 = temp-1;
+
+            for(int r=2; r <= p; r++){
+                System.out.print(--temp2 + " ");
+                temp2--;
+            }
+
+            System.out.println(" ");
+        }
+
+    }
+}
+//***** END OF PATTERN 50*****//
+
